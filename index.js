f122ae79f7160b82960a9d9345af4ee45ab585bb
sabrababra <sabrinayesminprome@gmail.com>
2022-11-25 23:45:53 +0600

    Complete basic setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const port= process.env.PORT || 5000;
+
+const app=express();
+
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/',async(req,res)=>{
+    res.send('bike resale server is running')
+})
+
+app.listen(port,()=>{
+    console.log('bike resale running on port', port);
+})
