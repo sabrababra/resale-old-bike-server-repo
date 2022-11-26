8d33ec0c6467c4f5d44e15650effb31881089d23
sabrababra <sabrinayesminprome@gmail.com>
2022-11-27 04:10:52 +0600

    create booking api

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const port= process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
-const app=express();
+const app = express();
 
 
 app.use(cors())
@@ -13,32 +13,51 @@ app.use(express.json())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}>@cluster0.r5wfiz1.mongodb.net/?retryWrites=true&w=majority`;
-//console.log(uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hmg27un.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-async function run(){
-    try{
-        const collectionBike=client.db('resale-bike').collection('bikeCollection')
-        app.get('/bikeCollection',async(req,res)=>{
-            const query={};
-            const options=await collectionBike.find(query).toArray();
-            res.send(options);
+async function run() {
+    try {
+        const bikesCollection = client.db('resaleBike').collection('bikeCollection');
+        const userCollection = client.db('resaleBike').collection('user');
+        const bookingCollection = client.db('resaleBike').collection('booking');
+
+        // user 
+        app.put('/user', async (req, res) => {
+            const user = req.body;
+            const result = await userCollection.updateOne(user);
+            res.send(result);
+        });
+
+        // add booking 
+        app.post('/addBooking', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        });
+
+        // get bike 
+        app.get('/allBikes', async (req, res) => {
+            const category = req.query.category;
+            const query = { category: category };
+            const bikes = await bikesCollection.find(query).toArray();
+            res.send(bikes);
         })
     }
-    finally{
+    finally {
 
     }
 }
 
-run.catch(console.log);
+run().catch(console.log);
 
 
-app.get('/',async(req,res)=>{
+app.get('/', async (req, res) => {
     res.send('bike resale server is running')
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('bike resale running on port', port);
 })
